184bd627248697eadfd76fafc54d12144afbe54e
cladlink <michael.boutboul87@gmail.com>
2016-02-01 17:20:20 +0100

    ajout d'une tentative de layeredPane infructueuse (code fonctionne avec un JPanel dans un JPanel)

---
+++ src/Carte.java
@@ -0,0 +1,15 @@
+/**
+ * Created by cladlink on 29/01/16.
+ */
+public class Carte
+{
+    private int north;
+    private int east;
+    private int south;
+    private int west;
+
+    private String adresse;
+
+    private Boolean isToPlayer;
+
+}
--- src/Vue.java
+++ src/Vue.java
@@ -1,16 +1,14 @@
 import javax.swing.*;
-import java.util.ArrayList;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
-/**
- * Created by cladlink on 21/01/16.
- */
 public class Vue extends JFrame
 {
-    private JLabel a;
+
     private ImageIcon[][] plateau;
-    private ArrayList<ImageIcon> cartesTeamA;
-    private ArrayList<ImageIcon> cartesTeamB;
+    private ImageIcon[] cartesTeamA;
+    private ImageIcon[] cartesTeamB;
     private String[] tabImageA =
             {
                 "Alice_Bleu.jpg",
@@ -27,36 +25,37 @@ public class Vue extends JFrame
                 "Idefix_Rouge.jpg",
                 "Pomme_Rouge.jpg"
             };
-   /* private static final ImageIcon[] SCORESBLEUS =
+    private ArrayList<JLabel> poolCarteA;
+    private ArrayList<JLabel> poolCarteB;
+    private static final ImageIcon[] SCORESBLEUS =
             {
-                    new ImageIcon(ImageIcon.class.getResource("img/scores/Score_Bleu_Mini_0.png")),
-                    new ImageIcon(ImageIcon.class.getResource("img/scores/Score_Bleu_Mini_1.png")),
-                    new ImageIcon(ImageIcon.class.getResource("img/scores/Score_Bleu_Mini_2.png")),
-                    new ImageIcon(ImageIcon.class.getResource("img/scores/Score_Bleu_Mini_3.png")),
-                    new ImageIcon(ImageIcon.class.getResource("img/scores/Score_Bleu_Mini_4.png")),
-                    new ImageIcon(ImageIcon.class.getResource("img/scores/Score_Bleu_Mini_5.png")),
-                    new ImageIcon(ImageIcon.class.getResource("img/scores/Score_Bleu_Mini_6.png")),
-                    new ImageIcon(ImageIcon.class.getResource("img/scores/Score_Bleu_Mini_7.png")),
-                    new ImageIcon(ImageIcon.class.getResource("img/scores/Score_Bleu_Mini_8.png")),
-                    new ImageIcon(ImageIcon.class.getResource("img/scores/Score_Bleu_Mini_9.png")),
-                    new ImageIcon(ImageIcon.class.getResource("img/scores/Score_Bleu_Mini_10.png"))
-
+                    new ImageIcon(Vue.class.getResource("img/scores/Score_Bleu_Mini_0.png")),
+                    new ImageIcon(Vue.class.getResource("img/scores/Score_Bleu_Mini_1.png")),
+                    new ImageIcon(Vue.class.getResource("img/scores/Score_Bleu_Mini_2.png")),
+                    new ImageIcon(Vue.class.getResource("img/scores/Score_Bleu_Mini_3.png")),
+                    new ImageIcon(Vue.class.getResource("img/scores/Score_Bleu_Mini_4.png")),
+                    new ImageIcon(Vue.class.getResource("img/scores/Score_Bleu_Mini_5.png")),
+                    new ImageIcon(Vue.class.getResource("img/scores/Score_Bleu_Mini_6.png")),
+                    new ImageIcon(Vue.class.getResource("img/scores/Score_Bleu_Mini_7.png")),
+                    new ImageIcon(Vue.class.getResource("img/scores/Score_Bleu_Mini_8.png")),
+                    new ImageIcon(Vue.class.getResource("img/scores/Score_Bleu_Mini_9.png")),
+                    new ImageIcon(Vue.class.getResource("img/scores/Score_Bleu_Mini_10.png"))
             };
     private static final ImageIcon[] SCORESROUGES =
             {
-                    new ImageIcon(ImageIcon.class.getResource("img/scores/Score_Rouge_Mini_0.png")),
-                    new ImageIcon(ImageIcon.class.getResource("img/scores/Score_Rouge_Mini_1.png")),
-                    new ImageIcon(ImageIcon.class.getResource("img/scores/Score_Rouge_Mini_2.png")),
-                    new ImageIcon(ImageIcon.class.getResource("img/scores/Score_Rouge_Mini_3.png")),
-                    new ImageIcon(ImageIcon.class.getResource("img/scores/Score_Rouge_Mini_4.png")),
-                    new ImageIcon(ImageIcon.class.getResource("img/scores/Score_Rouge_Mini_5.png")),
-                    new ImageIcon(ImageIcon.class.getResource("img/scores/Score_Rouge_Mini_6.png")),
-                    new ImageIcon(ImageIcon.class.getResource("img/scores/Score_Rouge_Mini_7.png")),
-                    new ImageIcon(ImageIcon.class.getResource("img/scores/Score_Rouge_Mini_8.png")),
-                    new ImageIcon(ImageIcon.class.getResource("img/scores/Score_Rouge_Mini_9.png")),
-                    new ImageIcon(ImageIcon.class.getResource("img/scores/Score_Rouge_Mini_10.png"))
-            };*/
-    private final ImageIcon empty = new ImageIcon(getClass().getResource(("img/Empty.jpg")));
+                    new ImageIcon(Vue.class.getResource("img/scores/Score_Rouge_Mini_0.png")),
+                    new ImageIcon(Vue.class.getResource("img/scores/Score_Rouge_Mini_1.png")),
+                    new ImageIcon(Vue.class.getResource("img/scores/Score_Rouge_Mini_2.png")),
+                    new ImageIcon(Vue.class.getResource("img/scores/Score_Rouge_Mini_3.png")),
+                    new ImageIcon(Vue.class.getResource("img/scores/Score_Rouge_Mini_4.png")),
+                    new ImageIcon(Vue.class.getResource("img/scores/Score_Rouge_Mini_5.png")),
+                    new ImageIcon(Vue.class.getResource("img/scores/Score_Rouge_Mini_6.png")),
+                    new ImageIcon(Vue.class.getResource("img/scores/Score_Rouge_Mini_7.png")),
+                    new ImageIcon(Vue.class.getResource("img/scores/Score_Rouge_Mini_8.png")),
+                    new ImageIcon(Vue.class.getResource("img/scores/Score_Rouge_Mini_9.png")),
+                    new ImageIcon(Vue.class.getResource("img/scores/Score_Rouge_Mini_10.png"))
+            };
+    private final static ImageIcon EMPTY = new ImageIcon(Vue.class.getResource("img/Empty.jpg"));
 
     public Vue (Model model)
     {
@@ -70,24 +69,44 @@ public class Vue extends JFrame
     }
     private void initAttribut()
     {
-
-        a = new JLabel("coucou");
-        int i, j;
-        String adresse = "img/scores/";
-        cartesTeamA = new ArrayList<>(5);
-        for(i=0; i<cartesTeamA.size(); i++)
+        int i;
+        String adresse;
+        // set cartes joueur A
+        cartesTeamA = new ImageIcon[5];
+        poolCarteA = new ArrayList<>();
+        for(i=0; i<tabImageA.length; i++)
         {
+            adresse = "img/cartes/";
             adresse += tabImageA[i];
-            System.out.println(adresse);
-            //cartesTeamA.add(i, new ImageIcon(ImageIcon.class.getResource(adresse )));
-            adresse = "";
+            cartesTeamA[i] = new ImageIcon(Vue.class.getResource(adresse));
+            cartesTeamA[i] = new ImageIcon(cartesTeamA[i].getImage().getScaledInstance(200, 200, BufferedImage.SCALE_SMOOTH));
+            poolCarteA.add(i, new JLabel(cartesTeamA[i]));
         }
-        cartesTeamB = new ArrayList<>(5);
+
+        // set cartes joueur B
+        cartesTeamB = new ImageIcon[5];
+        poolCarteB = new ArrayList<>();
+        for(i=0;i<tabImageB.length; i++)
+        {
+            adresse = "img/cartes/";
+            adresse += tabImageB[i];
+            cartesTeamB[i] = new ImageIcon (Vue.class.getResource(adresse));
+            cartesTeamB[i] = new ImageIcon(cartesTeamB[i].getImage().getScaledInstance(200, 200, BufferedImage.SCALE_SMOOTH));
+            poolCarteB.add(i, new JLabel(cartesTeamB[i]));
+        }
     }
+
     private void creerWidget()
     {
-        JPanel pano = new JPanel();
-        pano.add(a);
-        setContentPane(pano);
+        JPanel pCartesTeamA = new JPanel();
+        JLayeredPane lp = new JLayeredPane();
+        pCartesTeamA.setLayout(new BoxLayout(pCartesTeamA, BoxLayout.Y_AXIS));
+        for(int i=0; i<poolCarteA.size(); i++)
+        {
+            lp.add(poolCarteA.get(i), i);
+            lp.add(Box.createRigidArea(new Dimension(0, -150)));
+        }
+        pCartesTeamA.add(lp);
+        setContentPane(pCartesTeamA);
     }
 }
